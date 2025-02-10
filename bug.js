d108cc628064dc2d4e95330412dfe2616ab305b0
BugHunter-X <askzenai@gmail.com>
2025-02-10 13:32:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,26 @@
+```javascript
+// tailwind.config.js
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+
+```html
+<!-- index.html -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tailwind CSS Bug</title>
+  <link href="/dist/output.css" rel="stylesheet">
+</head>
+<body>
+  <div class="bg-red-500 p-4">
+    This div should have a red background.
+  </div>
+</body>
+</html>
+```
